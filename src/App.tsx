fdf7c150c33c16079c7d96a0b21bdd7fb2bce156
Samuel Rae <samuelrae@Samuels-Air.fritz.box>
2023-03-18 23:55:07 +0000

    feat: fetch book data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useEffect, useState } from 'react';
+import axios from 'axios';
+
+type BookDatum = {
+    cover_i: number;
+    has_fulltext: boolean;
+    edition_count: number;
+    title: string;
+    author_name: string[];
+    first_publish_year: 1955;
+    key: string;
+    ia: string[];
+    author_key: string[];
+    public_scan_b: boolean;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [searchTerm, setSearchTem] = useState('');
+    const [results, setResults] = useState<BookDatum[]>([]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        const query = event.target.value;
+        console.log({ query });
+        if (query && query.length > 1) {
+            setSearchTem(query);
+        }
+    }
+
+    useEffect(() => {
+        axios
+            .get<{ docs: BookDatum[] }>(
+                `https://openlibrary.org/search.json?q=${searchTerm}`
+            )
+            .then(({ data }) => setResults(data.docs));
+    }, [searchTerm]);
+
+    return (
+        <div className="App">
+            <h1>Vite + React</h1>
+            <label htmlFor="search">search</label>
+            <input id="search" onChange={handleChange} type="text"></input>
+            <ul>
+                {results.map((result) => {
+                    return <li>{result.title}</li>;
+                })}
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
